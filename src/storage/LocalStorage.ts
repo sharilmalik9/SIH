7cdcbec101ffe39d7774195ba5baca0069b67fd3
sharilmalik9 <sharil.malik.ug21@nsut.ac.in>
2023-09-12 12:32:50 +0530

    first commit

---
+++ src/storage/LocalStorage.ts
@@ -0,0 +1,89 @@
+import { Plugins } from "@capacitor/core";
+const { Storage } = Plugins;
+
+export class File {
+  created: string;
+  modified: string;
+  name: string;
+  content: string;
+  billType: number;
+
+  constructor(
+    created: string,
+    modified: string,
+    content: string,
+    name: string,
+    billType: number
+  ) {
+    this.created = created;
+    this.modified = modified;
+    this.content = content;
+    this.name = name;
+    this.billType = billType;
+  }
+}
+
+export class Local {
+  _saveFile = async (file: File) => {
+    let data = {
+      created: file.created,
+      modified: file.modified,
+      content: file.content,
+      name: file.name,
+      billType: file.billType,
+    };
+    await Storage.set({
+      key: file.name,
+      value: JSON.stringify(data),
+    });
+  };
+
+  _saveFileFir = async (file: File) => {
+    let data = {
+      created: file.created,
+      modified: file.modified,
+      content: file.content,
+      name: file.name,
+      billType: file.billType,
+    };
+    const res = await fetch('https://sih-inter2023-default-rtdb.firebaseio.com/form.json',{
+      method : 'POST',
+      headers :{
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify(data)
+    })
+  };
+
+  _getFile = async (name: string) => {
+    const rawData = await Storage.get({ key: name });
+    return JSON.parse(rawData.value);
+  };
+
+  _getAllFiles = async () => {
+    let arr = {};
+    const { keys } = await Storage.keys();
+    console.log(keys);
+    for (let i = 0; i < keys.length; i++) {
+      let fname = keys[i];
+      const data = await this._getFile(fname);
+      arr[fname] = (data as any).modified;
+    }
+    return arr;
+  };
+
+  _deleteFile = async (name: string) => {
+    await Storage.remove({ key: name });
+  };
+
+  _checkKey = async (key) => {
+    const { keys } = await Storage.keys();
+    if (keys.includes(key, 0)) {
+      return true;
+    } else {
+      return false;
+    } 
+  };
+}
+// firebase entry 
+
